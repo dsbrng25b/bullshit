7f61031e3e3cb948a689171ffbb1bb1ae5c4da47
David Schneider <dsbrng25b@gmail.com>
2021-01-23 20:39:16 +0100

    Add bullshit service

---
+++ cmd/baas/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dsbrng25b/bullshit"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	var (
+		tls  bool
+		host string
+	)
+
+	flag.BoolVar(&tls, "tls", false, "use acme to configure certificates and serve tls")
+	flag.StringVar(&host, "host", "", "hostname for the certificate")
+	flag.Parse()
+
+	http.HandleFunc("/", Bullshit)
+
+	s := &http.Server{
+		Addr: ":80",
+	}
+
+	if tls {
+		if host == "" {
+			log.Fatal("flag -host required with -tls")
+		}
+		m := &autocert.Manager{
+			Cache:      autocert.DirCache("acme"),
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist(host),
+		}
+		s.Addr = ":443"
+		s.TLSConfig = m.TLSConfig()
+		log.Fatal(s.ListenAndServeTLS("", ""))
+		return
+	}
+
+	log.Fatal(s.ListenAndServe())
+
+}
+
+func Bullshit(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+	fmt.Fprintf(w, bullshit.Get())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dsbrng25b/bullshit
 
 go 1.14
+
+require golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
